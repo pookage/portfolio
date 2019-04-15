20e4442f883bb7b8aca5c71c2e8d234603c87f48
pookage <pookagehayes@gmail.com>
2019-04-15 15:51:52 +0100

    text elements of the about page styled

--- src/react/components/About/About.jsx
+++ src/react/components/About/About.jsx
@@ -45,48 +45,47 @@ export default function About(){
 
 		return(
 			<article className={s.wrapper}>
-				<StickyQuickNav 
+				<StickyQuickNav
+					className={s.back} 
 					HTMLTag="div"
-					className={s.back}
 				/>
 				<div className={s.container}>
 					<section className={s.ahoy}>
 						<header>
-							<h1 className={font.title}>
+							<h1 className={`${font.title} ${s.title}`}>
 								Ahoy
 							</h1>
-							<p className={font.subtitle}>
+							<p className={`${font.subtitle} ${s.subtitle}`}>
 								With a decade of front-end development experience under my belt, I'm your man if you're looking to craft an accessible, adaptable, and delightful user-experience.
 							</p>
 						</header>
-						<p className={font.body}>
+						<p className={`${font.body} ${s.body}`}>
 							I obsess over perfecting animations, writing a11y-compliant markup, and faithfully implementing designs in a way that'll make your art department and I best buds.
 						</p>
-						<p className={font.body}>
+						<p className={`${font.body} ${s.body}`}>
 							If you're need a bit of extra muscle on a tight project, an injection of energy in this week's sprint, or just a bit of expertise to get your project off the ground, then get in touch! I'm open to all short / medium-term contracts of up to 6-months.
 						</p>
 					</section>
 					<section className={s.mentoring}>
-						<h1 className={font.subtitle}>
+						<h1 className={`${font.subtitle} ${s.subtitle}`}>
 							Let's Make WebDev Better
 						</h1>
-						<p className={font.body}>	
+						<p className={`${font.body} ${s.body}`}>	
 							I firmly believe in making webdev a kinder, more diverse community, and I'm particularly interested in facilitating the inclusion of under-represented demographics in our industry. I try to mentor two people each year from a zero-knowledge foundation all the way to their first role. 
 						</p>
-						<p className={font.body}>
+						<p className={`${font.body} ${s.body}`}>
 							If you’re looking for some help getting into this industry, this is nothing scary - there is absolutely no cost involved, and we can work to figure out a schedule that works with you, but the commitment level required would be:
 						</p>
 						<ul className={s.commitment}>
-							<li className={font.body}>
+							<li className={`${font.body} ${s.item}`}>
 								Two evenings a week to show you the ropes and go through any roadblocks (either in-person or via video-chat).
 							</li>
-							<li className={font.body}>
+							<li className={`${font.body} ${s.item}`}>
 								A few additional hours each week for self-study.
 							</li>
 						</ul>
-						<p className={font.body}>
-							Sound up your street? 
-							<a href="#contact">Get in touch</a>.
+						<p className={`${font.body} ${s.body}`}>
+							Sound up your street? <a href="#contact">Get in touch</a>.
 						</p>
 					</section>
 					<figure
@@ -96,11 +95,13 @@ export default function About(){
 							{languages.map(renderLanguage)}
 						</ul>
 					</figure>
-					<img 
-						className={s.portrait}
-						src="Assets/photos/portrait.jpg"
-						alt="Portrait of Pookage Hayes, smiling with his elbows on the desk."
-					/>
+					<div className={s.portrait}>
+						<img 
+							className={s.image}
+							src="Assets/photos/portrait.jpg"
+							alt="Portrait of Pookage Hayes, smiling with his elbows on the desk."
+						/>
+					</div>
 				</div>
 			</article>
 		);
