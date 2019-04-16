6336c93153d5483c2eb45d79dc2121db2f341fac
pookage <pookagehayes@gmail.com>
2019-04-16 20:50:34 +0100

    removed unnecessary App component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { PageProvider } from "Contexts/Page.js";
 import KeyboardControls from "ReactComponents/KeyboardControls/KeyboardControls.jsx";
+import BedroomScene from "ReactComponents/BedroomScene/BedroomScene.jsx";
+import Portfolio from "ReactComponents/Portfolio/Portfolio.jsx";
+import About from "ReactComponents/About/About.jsx";
+import Contact from "ReactComponents/Contact/Contact.jsx";
 import App from "ReactComponents/App/App.jsx";
 import "Shared/global.css";
 
@@ -12,7 +16,10 @@ function init(){
 		(
 			<PageProvider>
 				<KeyboardControls>
-					<App />
+					<BedroomScene />
+					<Portfolio />
+					<About />
+					<Contact/>
 				</KeyboardControls>
 			</PageProvider>
 		),
